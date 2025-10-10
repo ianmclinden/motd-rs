cb853dad5fe50ef0ab82e3d7d3724de2434f72d9
Ian McLinden <mclinden.ian@gmail.com>
2025-10-10 12:09:29 -0500

    Update motd processing to tokio::Command

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,53 @@
 use clap::Parser;
+use futures::future::join_all;
 use std::{
     path::{Path, PathBuf},
-    process::{exit, Command},
-    sync::OnceLock,
+    process::exit,
+    sync::LazyLock,
 };
+use tokio::process::Command;
 use walkdir::WalkDir;
 
-fn motd_dir() -> &'static PathBuf {
-    static MOTD_DIR: OnceLock<PathBuf> = OnceLock::new();
-    MOTD_DIR.get_or_init(|| {
-        let prefix = match option_env!("PREFIX") {
-            Some(v) if !v.is_empty() => v,
-            _ => "/",
-        };
-        Path::new(prefix).join("etc").join("update-motd.d")
-    })
-}
+static MOTD_DIR: LazyLock<PathBuf> = LazyLock::new(|| {
+    let prefix = match option_env!("PREFIX") {
+        Some(v) if !v.is_empty() => v,
+        _ => "/",
+    };
+    Path::new(prefix).join("etc").join("update-motd.d")
+});
 
-fn help_long() -> &'static String {
-    static HELP_LONG: OnceLock<String> = OnceLock::new();
-    HELP_LONG.get_or_init(|| {
-        format!(
-            "Dynamic message of the day generation.
+static HELP_LONG: LazyLock<String> = LazyLock::new(|| {
+    format!(
+        "Dynamic message of the day generation.
 
-Any executable scripts in '{}/*' are 
+Any executable scripts in '{}/*' are
 executed as the current user, and concatenated to the MOTD. These scripts must
 be executable, and must emit information on standard out.",
-            motd_dir().to_string_lossy()
-        )
-    })
-}
+        MOTD_DIR.display()
+    )
+});
 
 /// Dynamic MOTD generation
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = help_long())]
+#[command(version, about, long_about = HELP_LONG.as_str())]
 struct Args {
-    /// Print the MOTD_DIR path and exit
+    /// Print the `MOTD_DIR` path and exit
     #[arg(short, long, default_value_t = false)]
     path: bool,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
 
     // Path only
     if args.path {
-        println!("{}", motd_dir().to_string_lossy());
+        println!("{}", MOTD_DIR.display());
         exit(0);
     }
 
-    let mut motd_buf: String = String::new();
-
     // Execute and concat MOTD fragments
-    for fragment in WalkDir::new(motd_dir())
+    let motd_fragments = WalkDir::new(MOTD_DIR.as_path())
         .follow_links(true)
         .sort_by_file_name()
         .into_iter()
@@ -62,12 +57,10 @@ fn main() {
                 && e.metadata().unwrap().is_file()
                 && !e.file_name().to_string_lossy().ends_with(".default")
         })
-    {
-        // Lazily append only successes
-        if let Ok(output) = Command::new(fragment.into_path()).output() {
-            motd_buf.push_str(&String::from_utf8_lossy(&output.stdout));
-        }
-    }
+        .map(|f| Command::new(f.path()).output());
 
-    print!("{motd_buf}");
+    // Join output is ordered, keep output from successes
+    for output in (join_all(motd_fragments).await).into_iter().flatten() {
+        print!("{}", String::from_utf8_lossy(&output.stdout));
+    }
 }
