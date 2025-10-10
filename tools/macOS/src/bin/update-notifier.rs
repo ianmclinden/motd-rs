e96187fb96b65e424f3e5af7d951a66cf1201ebe
Ian McLinden <mclinden.ian@gmail.com>
2025-10-10 18:30:57 -0500

    Update macOS/update-notifier for Rust 2024

--- tools/macOS/src/bin/update-notifier.rs
+++ tools/macOS/src/bin/update-notifier.rs
@@ -3,45 +3,36 @@ use std::{
     fs,
     io::Write,
     path::{Path, PathBuf},
-    process::{exit, Command},
-    sync::OnceLock,
+    process::{Command, exit},
+    sync::LazyLock,
 };
 
-fn stamp_dir() -> &'static PathBuf {
-    static STAMP_DIR: OnceLock<PathBuf> = OnceLock::new();
-    STAMP_DIR.get_or_init(|| {
-        let prefix = match option_env!("PREFIX") {
-            Some(v) if !v.is_empty() => v,
-            _ => "/",
-        };
-        Path::new(prefix)
-            .join("var")
-            .join("lib")
-            .join("update-notifier")
-    })
-}
+static STAMP_DIR: LazyLock<PathBuf> = LazyLock::new(|| {
+    let prefix = match option_env!("PREFIX") {
+        Some(v) if !v.is_empty() => v,
+        _ => "/",
+    };
+    Path::new(prefix)
+        .join("var")
+        .join("lib")
+        .join("update-notifier")
+});
 
-fn stamp_file() -> &'static PathBuf {
-    static STAMP_FILE: OnceLock<PathBuf> = OnceLock::new();
-    STAMP_FILE.get_or_init(|| stamp_dir().join("updates-available"))
-}
+static STAMP_FILE: LazyLock<PathBuf> = LazyLock::new(|| STAMP_DIR.join("updates-available"));
 
-fn help_long() -> &'static String {
-    static HELP_LONG: OnceLock<String> = OnceLock::new();
-    HELP_LONG.get_or_init(|| {
-        format!(
-            "Track available brew updates for message of the day.
+static HELP_LONG: LazyLock<String> = LazyLock::new(|| {
+    format!(
+        "Track available brew updates for message of the day.
 
 When called, stampfile '{}' is updated with the current state
 of packages. This stampfile can then be read by an MOTD fragment.",
-            stamp_file().to_string_lossy()
-        )
-    })
-}
+        STAMP_FILE.display()
+    )
+});
 
-/// "Track available brew updates for MOTD"
+/// Track available brew updates for MOTD
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = help_long())]
+#[command(version, about, long_about = HELP_LONG.as_str())]
 struct Args {
     /// Print the update stampfile path and exit
     #[arg(short, long, default_value_t = false)]
@@ -53,7 +44,7 @@ fn main() {
 
     // Path only
     if args.path {
-        println!("{}", stamp_file().to_string_lossy());
+        println!("{}", STAMP_FILE.display());
         exit(0);
     }
 
@@ -62,8 +53,9 @@ fn main() {
         // Then, get outdated package list
         if let Ok(output) = Command::new("brew").arg("outdated").output() {
             // Create the update directory
-            fs::create_dir_all(stamp_dir()).expect("Could not create stamp directory.");
-            let mut stamp = fs::File::create(stamp_file()).expect("Could not create stampfile");
+            fs::create_dir_all(STAMP_DIR.as_path()).expect("Could not create stamp directory.");
+            let mut stamp =
+                fs::File::create(STAMP_FILE.as_path()).expect("Could not create stampfile");
 
             let outdated = String::from_utf8_lossy(&output.stdout).lines().count();
             if outdated > 0 {
